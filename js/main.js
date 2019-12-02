6586ae24b510de8ad94f79ead30317c625bb3daa
KOSaber <k.o.saber@hotmail.com>
2019-12-02 14:04:18 +0300

    left diagonal win

--- js/main.js
+++ js/main.js
@@ -3,12 +3,14 @@ window.onload = function () {
    
 
 var turn = 0;
+var countertie=0;
 
 
 function play(divnumber){
 
  var id = divnumber.attr("id");
  var newid= "#"+id;
+ 
 
 for(i=1; i<=4 ; i++){
 
@@ -18,18 +20,78 @@ for(i=1; i<=4 ; i++){
   
       if(turn==0){
         $(`${newid} > span:nth-child(`+ i +`)`).addClass(" red");
+        countertie +=1;
+
+        //chick winner
+        var winnerc=0;
+        var winnerr=0;
+
+        for(j=1;j<=4;j++){
+          //----check win col
+          if($(`${newid} > span:nth-child(`+ j +`)` ).hasClass("cir red"))
+          {
+            winnerc +=1;
+          }
+          //-----check win row
+          if($(`${"#colum"}`+ j + ` > span:nth-child(`+ i +`)`).hasClass("cir red"))
+          {
+            winnerr +=1;
+          }
+
+        }
+        if((winnerc==4) || (winnerr==4))
+        {
+          setTimeout(function(){ alert("you won!!"); }, 200);
+          setTimeout(function(){ reset(); }, 300);
+          // alert("you won!!"); 
+          // reset();
+        }
+
+        // -------------
         changeturn();
-        // here check code
                  }
       else if(turn==1) {
         $(`${newid} > span:nth-child(`+ i +`)`).addClass(" yellow");
+        countertie +=1;
+        // chick win
+        winnerc=0;
+        winnerr=0;
+
+        for(j=1;j<=4;j++){
+          //==== ckick win col
+          if($(`${newid} > span:nth-child(`+j+`)` ).hasClass("cir yellow"))
+          {
+            winnerc += 1;
+          }
+          //-----check win row
+          if($(`${"#colum"}`+ j + ` > span:nth-child(`+ i +`)`).hasClass("cir yellow"))
+          {
+            winnerr +=1;
+          }
+
+        }
+        if((winnerc==4) || (winnerr==4)){
+          
+          setTimeout(function(){ alert("Computer won!!"); }, 200);
+          setTimeout(function(){ reset(); }, 300);
+          // alert("Computer won!!");
+          // reset();
+        }
+        
+
         changeturn();
-        // here check code
+        
             }
 break;
 
     }
                     }
+
+    if(countertie == 16)
+    {
+      setTimeout(function(){ alert("it's tie!!"); }, 300);
+      // alert("it's tie!!");
+    }
                        } 
 
 
@@ -63,12 +125,21 @@ changeturn();
       play($(this));
     });
 
-    $("#normalbutton").click(function(){
+    function reset(){
+  
       $("[class*='cir']").removeClass("red yellow");
       $(".playerdot").removeClass("red");
       $(".computerdot").removeClass("yellow");
-      changeturn();
-    });
+      countertie=0;
+      setTimeout(function(){ changeturn(); }, 500);
+
+    }
+
+  $("#restartbutton").click(function(){
+    reset();
+  });
+    
   
+
 
   }
